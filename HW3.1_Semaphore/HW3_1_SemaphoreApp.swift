a815b2bf026d00981781953436924bf1d7f73e8f
AlexeyBH <alexey_bh@mail.ru>
2022-01-20 08:40:22 +0300

    Initial Commit

---
+++ HW3.1_Semaphore/HW3_1_SemaphoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_1_SemaphoreApp.swift
+//  HW3.1_Semaphore
+//
+//  Created by Alexey Khestanov on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW3_1_SemaphoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
